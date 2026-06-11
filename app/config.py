4f0270516c33b3bd770395284db9134f34388229
BbyFAy16 <pstafsen@yahoo.com>
2026-06-11 11:39:20 +0100

    Update DATABASE_URL for production environment

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     # Database
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/course_enrollment"
+    DATABASE_URL: str = "postgresql://course_enrollment_jsrz_user:GPg6sLzV5oA3OVPxMr68rxK5iR8zF3s6@dpg-d8l8uk6rnols73cgijv0-a.frankfurt-postgres.render.com/course_enrollment_jsrz"
     
     # JWT
     SECRET_KEY: str = "011f104c44d55a0f8f421ba478624f77c82c9e01bd9f7ab0b13c4cd4796ad2a9"
