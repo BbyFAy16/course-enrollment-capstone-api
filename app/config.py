636cff01dc4147913d6457fb7b56c3b214ea2a11
BbyFAy16 <pstafsen@yahoo.com>
2026-06-11 11:27:37 +0100

    Change SECRET_KEY in config.py
    Updated SECRET_KEY for production use.

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/course_enrollment"
     
     # JWT
-    SECRET_KEY: str = "your-secret-key-change-in-production"
+    SECRET_KEY: str = "011f104c44d55a0f8f421ba478624f77c82c9e01bd9f7ab0b13c4cd4796ad2a9"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
