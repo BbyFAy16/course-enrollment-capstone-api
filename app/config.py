d190f94479bdae79ff734cb25c7208f2d1e54d28
BbyFAy16 <pstafsen@yahoo.com>
2026-09-02 13:30:07 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     # Database
-    DATABASE_URL: str = "postgresql://username:password@hostname:5432/databasename?sslmode=require"
+    DATABASE_URL: str = "postgresql://course_569l_user:7rPGWZdd1SoI19r584gliHOT5hRR9KBv@dpg-dac1d62jnfac73el39sg-a.frankfurt-postgres.render.com/course_569l"
     
     # JWT
     SECRET_KEY: str = "011f104c44d55a0f8f421ba478624f77c82c9e01bd9f7ab0b13c4cd4796ad2a9"
